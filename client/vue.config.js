8bbf9eab6e4e420a12efc6f06c8af6ea1ca9ead0
tzbcf <714953218@qq.com>
2018-12-17 08:47:07 +0800

    ‘配置修改’

--- client/vue.config.js
+++ client/vue.config.js
@@ -10,45 +10,58 @@
  * -----
  * Copyright (c) 2018 Huazhi Corporation. All rights reserved.
  */
-// vue.config.js
 module.exports = {
-    // 选项...
-    // 当使用基于 HTML5 history.pushState 的路由时；
-    // 当使用 pages 选项构建多页面应用时。
-    baseUrl:"",
-    // 当运行 vue-cli-service build 时生成的生产环境构建文件的目录。注意目标目录在构建之前会被清除 (构建时传入 --no-clean 可关闭该行为)。
-    outputDir:"webApp",
-    // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
-    assetsDir:"static",
-    // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
-    indexPath:"index.html",
-    // 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。然而，这也要求 index 的 HTML 是被 Vue CLI 自动生成的。如果你无法使用 Vue CLI 生成的 index HTML，你可以通过将这个选项设为 false 来关闭文件名哈希。
-    filenameHashing:true,
-    // 多页面
-    pages:undefined,
-    // 编译警告
-    lintOnSave:false,
-    // 是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
-    runtimeCompiler:false,
-    // 默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。
-    transpileDependencies:[],
-    // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
-    productionSourceMap:false,
-    // 设置生成的 HTML 中 <link rel="stylesheet"> 和 <script> 标签的 crossorigin 属性。需要注意的是该选项仅影响由 html-webpack-plugin 在构建时注入的标签 - 直接写在模版 (public/index.html) 中的标签不受影响。
-    crossorigin:undefined,
-    // 在生成的 HTML 中的 <link rel="stylesheet"> 和 <script> 标签上启用 Subresource Integrity (SRI)。如果你构建后的文件是部署在 CDN 上的，启用该选项可以提供额外的安全性。需要注意的是该选项仅影响由 html-webpack-plugin 在构建时注入的标签 - 直接写在模版 (public/index.html) 中的标签不受影响。另外，当启用 SRI 时，preload resource hints 会被禁用，因为 Chrome 的一个 bug 会导致文件被下载两次。
-    integrity:false,
-    // 反向代理
-    devServer:{
-        // devServer: {
-        //     proxy: {
-        //       '/api': {
-        //         target: '1',
-        //         ws: true,
-        //         changeOrigin: true
-        //       }
-        //     }
-        // }
+    // 基本路径
+    baseUrl: '/',
+    // 输出文件目录
+    outputDir: 'dist',
+    // eslint-loader 是否在保存的时候检查
+    lintOnSave: true,
+    // use the full build with in-browser compiler?
+    // https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
+    compiler: false,
+    // webpack配置
+    // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+    chainWebpack: () => { },
+    configureWebpack: () => { },
+    // vue-loader 配置项
+    // https://vue-loader.vuejs.org/en/options.html
+    vueLoader: {},
+    // 生产环境是否生成 sourceMap 文件
+    productionSourceMap: true,
+    // css相关配置
+    css: {
+        // 是否使用css分离插件 ExtractTextPlugin
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {},
+        // 启用 CSS modules for all css / pre-processor files.
+        modules: false
+    },
+    // use thread-loader for babel & TS in production build
+    // enabled by default if the machine has more than 1 cores
+    parallel: require('os').cpus().length > 1,
+    // 是否启用dll
+    // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#dll-mode
+    dll: false,
+    // PWA 插件相关配置
+    // see https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
+    pwa: {},
+    // webpack-dev-server 相关配置
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        proxy: null, // 设置代理
+        before: app => { }
+    },
+    // 第三方插件配置
+    pluginOptions: {
+        // ...
     }
 }
 
